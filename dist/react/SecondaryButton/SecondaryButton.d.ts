6a34f4f38ac816628589b65ec204d7f402ca8b66
Morteza (Laptop) <bagheri1382morteza@gmail.com>
2024-11-25 20:52:51 +0330

    Update telegram-web-apps.js

---
+++ dist/react/SecondaryButton/SecondaryButton.d.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { BottomButtonProps } from "../bottomButton";
+import { SecondaryButton as SecondaryButtonType } from "@twa-dev-mba/types";
+export declare const SecondaryButton: FC<BottomButtonProps & {
+    position?: SecondaryButtonType["position"];
+}>;
---
+++ dist/sdk.d.ts
@@ -0,0 +1,3 @@
+import "./telegram-web-apps";
+import { WebApp as WebAppTypes } from "@twa-dev-mba/types";
+export declare const WebApp: WebAppTypes;
